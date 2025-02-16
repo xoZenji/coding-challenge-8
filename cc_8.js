d9ef2decf213b58f696e5dccfb28bb9a02b914a2
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-16 17:34:06 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -17,3 +17,14 @@ const calculateDiscount = function(price, discountRate) {
 console.log(calculateDiscount(100, 0.2));  // Expected output: "Final Price: $80.00"
 console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50"
 // Prints the final price after the discount is applied to the console.
+
+// Task 3: Service Fee Calculation
+let calculateServiceFee = (amount, serviceType) => {
+    let rateFee = serviceType === "Premium" ? 0.15: serviceType === "Standard" ? 0.10 : serviceType === "Basic" ? 0.05 : 0;
+    let servicefee = amount * rateFee;
+        return `Service Fee: $${servicefee.toFixed(2)}`;
+}; // Created a function that calculates the service fee based on the service type.
+
+console.log(calculateServiceFee(200, "Premium")); // Expected output: "Service Fee: $30.00"
+console.log(calculateServiceFee(500, "Standard")); // Expected output: "Service Fee: $50.00"
+// Prints the service fee for both standard and premium in the console.
